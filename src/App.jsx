6596afa9f6749d63d94fcbad4c56d7324a094089
senthil-k8s <senthilathiban0@gmail.com>
2024-01-29 17:56:12 +0530

    feat(dasboard): Sidbar Implemented

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header'
+import Sidebar from './components/sidebar'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='flex flex-col'>
+        <Sidebar />
+      </div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+import { Schedule, Calendar, Setting, Ticket, Category, Notification, Upload, Logo, Base } from "../assets"
+import Header from "./Header"
+import Menu from "./Menu"
+
+const Sidebar = () => {
+    // let fields = [
+    //     {
+    //         title: "Dashboard",
+    //         icon: Category,
+    //     },
+    //     {
+    //         title: "Upload",
+    //         icon: Upload,
+    //     },
+    //     {
+    //         title: "Invoice",
+    //         icon: Ticket,
+    //     },
+    //     {
+    //         title: "Schedule",
+    //         icon: Schedule,
+    //     },
+    //     {
+    //         title: "Calender",
+    //         icon: Calendar,
+    //     },
+    //     {
+    //         title: "Notification",
+    //         icon: Notification,
+    //     },
+    //     {
+    //         title: "Settings",
+    //         icon: Setting,
+    //     },
+    // ]
+    return (
+        <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white  text-black dark:text-white">
+            <Header />
+            <Menu />
+        </div>
+    )
+}
+
+export default Sidebar
